e8c581324f8a4f625898b593b7956fe910720b85
Avijit <abhijitpradhan909@gmail.com>
2025-09-21 10:28:07 +0530

    feat:log req

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
+  console.log(JSON.stringify(c.req, null, 2));
   return c.json({
     status: 200,
     message: "Hello From Bun Hono server",
