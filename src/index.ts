a3aac25064fa2bb05f9e3256013a7a4677ad6527
Avijit <abhijitpradhan909@gmail.com>
2025-09-21 11:49:11 +0530

    feat:get all build file as per project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { Hono } from "hono";
 
 const app = new Hono();
+const s3 = new S3Client({
+  region: "auto",
+  forcePathStyle: true,
+  endpoint: process.env.R2_ENDPOINT || "",
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || "",
+  },
+});
 
 app.get("/", (c) => {
   console.log(JSON.stringify(c.req, null, 2));
@@ -13,9 +23,26 @@ app.get("/", (c) => {
 app.get("/resolve", async (c) => {
   const project = c.req.query("project");
   const commit = c.req.query("commit");
-  return c.json({
-    project,
-    commit,
-  });
+  const reqPath = c.req.path === "/" ? "/index.html" : c.req.path;
+
+  try {
+    const result = await s3.send(
+      new GetObjectCommand({
+        Bucket: process.env.R2_BUCKET_NAME,
+        Key: `${project}/${reqPath}`,
+      })
+    );
+    return new Response(result.Body as ReadableStream, {
+      headers: {
+        "Content-Type": result.ContentType || "text/plain",
+      },
+      status: result.$metadata.httpStatusCode,
+    });
+  } catch (error) {
+    return c.json({
+      status: 404,
+      message: "Not Found",
+    });
+  }
 });
 export default app;
